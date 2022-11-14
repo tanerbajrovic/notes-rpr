1a27e560bf98bc234ed310a50c7ab4a3bcc6657a
Taner Bajrovic <tanerbajrovic@gmail.com>
2022-11-14 11:55:26 +0100

    Homework 1: Added JUnit5 and JavaDoc plugin

---
+++ zadace/zadaca-1/App/src/main/java/ba/unsa/etf/rpr/App.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+public class App {
+    /**
+     * Razvoj programskih rješenja, 2022
+     * Zadaca #1
+     * @author Taner Bajrović
+     */
+    public static void main(String[] args) {
+
+    }
+}
